9d1760948c5172ea17583a0acd49074b4cadec20
David House <david.house@kp.org>
2019-09-21 21:31:16 -0400

    :construction: lint and test fixes

---
+++ test/stampede-worker-tests.js
@@ -0,0 +1,12 @@
+'use strict'
+/* eslint-env mocha */
+const expect = require('chai').expect
+
+describe('Stampede Worker', function() {
+  beforeEach(() => {
+  })
+
+  it('should be tested', function() {
+    expect(true).to.equal(true)
+  })
+})
